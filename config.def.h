b9b8718fa73c1be531ce79c32b27956528ebbe66
Ron <rumbletumjum@gmail.com>
2020-09-18 13:08:52 -0400

    20200918T1308

--- config.def.h
+++ config.def.h
@@ -2,26 +2,30 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 8;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Misc Tamsyn:size=8:antialias=false:autohint=false" };
-static const char dmenufont[]       = "Misc Tamsyn:size=8:antialias=false:autohint=false"; 
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Hack:size=10:antialias=true:autohint=true" };
+static const char dmenufont[]       = "Hack:size=10:antialias=true:autohint=true";
+static const char col_gray1[]       = "#2e3440";
+static const char col_gray2[]       = "#2e3440";
+static const char col_gray3[]       = "#d8dee9";
+static const char col_gray4[]       = "#d8dee9";
+static const char col_cyan[]        = "#4c566a";
 static const char col_red[]         = "#770055";
 static const char *colors[][3]      = {
+  [SchemeNorm] = { "#373b41", "#1d1f21", "#282a2e",}, // 1 = normal (grey on black)
+  [SchemeSel]  = { "#c5c8c6", "#1d1f21", "#f0c674",}, // 2 = selected (white on black)
+};
 	/*               fg         bg         border   */
+/*	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 	[SchemeNorm] = { "#373b41", "#1d1f21", "#c5c8c6" },
 	[SchemeSel]  = { "#c5c8c6", "#1d1f21", "#6699cc" },
-/*
+
 	 * [SchemeNorm] = { "#e6e1cf", "#1f2430", "#888888" },
 	 * [SchemeSel]  = { "#e6e1cf", "#212121", "#e6e1cf"   },
 */
-};
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
